ff2e9ec650170e8794121bde9a5693a03ee77658
“jaimediaz0612” <jaimehernandiazm@gmail.com>
2017-09-09 17:02:25 -0500

    Funciona!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule, Routes} from '@angular/router';
+
 import { AppComponent } from './app.component';
+
+import {CustomersModule} from './customers/customers.module';
+import {OrdersModule} from './orders/orders.module';
+
+import { MenuComponent } from './menu/menu.component';
+import { CurrentOptionComponent } from './current-option/current-option.component';
+
+import { IndexComponent } from './customers/index/index.component';
+import {AddComponent as OrderAddComponent } from './orders/add/add.component';
+
+const appRoutes: Routes = [
+  { path: 'customers/index', component:IndexComponent },
+  { path: 'orders/add',      component: OrderAddComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CurrentOptionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CustomersModule,
+    OrdersModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
