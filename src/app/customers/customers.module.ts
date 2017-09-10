2f5b17ded1788f5e7076b05ea5ef6e1ed2951c22
“jaimediaz0612” <jaimehernandiazm@gmail.com>
2017-09-10 10:53:17 -0500

    Estilos

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index/index.component';
+import { AddComponent } from './add/add.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [IndexComponent]
+  declarations: [IndexComponent, AddComponent, ListComponent]
 })
 export class CustomersModule { }
